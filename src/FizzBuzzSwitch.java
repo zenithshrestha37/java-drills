16054c7afdff7493d6350edc6991003fb85c1c2d
zenithshrestha37 <zenithshrestha37@gmail.com>
2021-08-04 13:29:10 -0500

    switch fizzbuzz added

--- src/FizzBuzzSwitch.java
+++ src/FizzBuzzSwitch.java
@@ -5,15 +5,32 @@ public class FizzBuzzSwitch {
     }
 
     private static void fizzBuzz100() {
+
         for (int i = 1; i <= 100; i++) {
-            if ((i % 3) == 0)
-                System.out.println("fizz");
-            else if ((i % 5) == 0)
-                System.out.println("buzz");
-            else if (((i % 5) == 0) && ((i % 3) == 0))
-                System.out.println("fizzbuzz");
-            else
-                System.out.println(i);
+            int test = 0;
+            if (i % 3 == 0) {
+                test = 1;
+            } else if (i % 5 == 0) {
+                test = 2;
+            } else if (i % 5 == 0 && i % 3 == 0) {
+                test = 3;
+            }
+
+            switch (test) {
+                case 1:
+                    System.out.println("fizz");
+                    break;
+                case 2:
+                    System.out.println("buzz");
+                    break;
+                case 3:
+                    System.out.println("fizzbuzz");
+                    break;
+                default:
+                    System.out.println(i);
+                    break;
+            }
         }
     }
 }
+
